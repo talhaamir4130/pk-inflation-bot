788584850d1d5cde21c9844e98bf23451fc5929c
Talha Amir <talhaamir2014@gmail.com>
2022-08-02 20:00:48 +0500

    parse data

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World')
+from calendar import calendar
+import requests
+from bs4 import BeautifulSoup
+
+print('Get Trading Economics page')
+page = requests.get('https://tradingeconomics.com/pakistan/inflation-cpi', headers={'User-Agent': 'Mozilla/5.0'})
+
+html = page.text
+
+print('Creating Parse Object')
+soup = BeautifulSoup(html, 'html.parser')
+
+calendar = soup.find(id='calendar')
+
+print(calendar.text)
+
+print('Done')
